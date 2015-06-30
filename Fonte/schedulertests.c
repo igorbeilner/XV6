bb465fa21a9a9e89876b85c4bc2d58805bde9e94
igorbeilner <igor.beilner@hotmail.com>
2015-06-30 19:44:31 -0300

    Adição de testes em outro arquivo

---
+++ Fonte/schedulertests.c
@@ -0,0 +1,35 @@
+#include "types.h"
+#include "param.h"
+#include "stat.h"
+#include "user.h"
+
+#define MAX_TESTE 9000
+#define MAX_PROCESS 10
+
+void teste(){
+  int i, j;
+    for(i=0; i<MAX_TESTE; i++)
+      for(j=0; j<MAX_TESTE; j++);
+}
+
+int main() {
+  int i, vet_pid[MAX_PROCESS];
+
+  for(i=1; i<=MAX_PROCESS; i++) {
+
+    vet_pid[i] = fork(i);
+
+    if (vet_pid[i] == 0){
+      teste();
+      exit();
+    }
+
+  }
+
+  printf(1, "     name      |   pid    |   step   |  stride  |   CPU\n");
+  for(i=0; i<MAX_PROCESS; i++)
+    wait();
+  exit();
+
+  return 0;
+}
